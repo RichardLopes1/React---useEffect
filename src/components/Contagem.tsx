dd419789be87565e839b7234a889e58d7a5c3bf6
React---useEffect <richardlopes348@gmail.com>
2024-09-18 08:40:32 -0300

    primeiro commit

--- src/components/Contagem.tsx
+++ src/components/Contagem.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 type CountProps = {
     count: number;
@@ -7,6 +7,8 @@ type CountProps = {
 
 
 export default function Contagem({ count, aumentar }: CountProps) {
+
+    const [valor, setValor] = useState(0)
 
     //chamado em todas as fases do ciclo de vida.
     useEffect(() => {
@@ -28,7 +30,9 @@ export default function Contagem({ count, aumentar }: CountProps) {
         <div>
             <h2>Contagem</h2>
             <p>Valor de count = {count}</p>
+            <p>Valor do valor= {valor}  </p>
             <button onClick={aumentar}>Aumentar</button>
+            <button onClick={() => setValor(valor + 1)}>aumentar valor</button>
         </div>
     )
 }
